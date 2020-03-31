68f643b97200f9329d885e3722164c4a3b08285f
Victor Tsang <victorwltsang@gmail.com>
2020-03-31 12:39:34 -0700

    1 foot forward

---
+++ src/11ty/_data/siteAttributes.js
@@ -0,0 +1,12 @@
+module.exports = {
+	"": {
+		metaTitle: "Portfolio | Victor Tsang",
+		metaDescription: "My portfolio",
+		bodyClass: "home",
+	},
+	about: {
+		metaTitle: "About Me | Victor Tsang",
+		metaDescription: "About Me",
+		bodyClass: "about",
+	},
+}
