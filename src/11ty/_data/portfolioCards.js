13c651ceb97bd3bb287bfa3b1b566e56f2887acc
Victor Tsang <victorwltsang@gmail.com>
2020-04-01 17:11:00 -0700

    breakpoint looks good

---
+++ src/11ty/_data/portfolioCards.js
@@ -0,0 +1,38 @@
+module.exports = [
+	{
+		id: 1,
+		title: "NetBase On-Tour",
+		description: "Event microsite for company's tradeshows and conferences",
+		imageUrl:
+			"https://www.personalcapital.com/static/images/rebrand/heros/1440/hero-home-tools-2-v4.png",
+		url: "netbase-on-tour",
+		bgColor: "bg-red-200",
+	},
+	{
+		id: 1,
+		title: "NetBase On-Tour",
+		description: "Event microsite for company's tradeshows and conferences",
+		imageUrl:
+			"https://www.personalcapital.com/static/images/rebrand/heros/1440/hero-home-tools-2-v4.png",
+		url: "netbase-on-tour",
+		bgColor: "bg-purple-200",
+	},
+	{
+		id: 1,
+		title: "NetBase On-Tour",
+		description: "Event microsite for company's tradeshows and conferences",
+		imageUrl:
+			"https://www.personalcapital.com/static/images/rebrand/heros/1440/hero-home-tools-2-v4.png",
+		url: "netbase-on-tour",
+		bgColor: "bg-green-200",
+	},
+	{
+		id: 1,
+		title: "NetBase On-Tour",
+		description: "Event microsite for company's tradeshows and conferences",
+		imageUrl:
+			"https://www.personalcapital.com/static/images/rebrand/heros/1440/hero-home-tools-2-v4.png",
+		url: "netbase-on-tour",
+		bgColor: "bg-blue-200",
+	},
+];
