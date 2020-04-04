53901dfea5d6b6bda3d125b99e5d8d128aa5f639
Victor Tsang <victorwltsang@gmail.com>
2020-04-04 15:51:55 -0700

    pugsum

--- src/11ty/_data/portfolioList.js
+++ src/11ty/_data/portfolioList.js
@@ -1,10 +1,10 @@
 module.exports = [
 	{
-		title: "NetBase On-Tour",
-		description: "Event microsite for company's tradeshows and conferences",
-		imageUrl:
-			"https://www.mamp.info/__we_thumbs__/1284_4_MAMP-PRO-MacBook-Pro.png",
-		url: "/netbase-on-tour",
+		title: "Pugsum",
+		description:
+			"When you combine pug and possum(11ty) to make something awesome, a 11ty portfolio using Pug templates",
+		imageUrl: "/assets/images/pugsum/thumbnail.png",
+		url: "/pugsum",
 		bgColor: "bg-blue-200",
 	},
 	{
