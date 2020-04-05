52346cd8b1a3cf3d648bdec27ee1cc1618158f15
Victor Tsang <victorwltsang@gmail.com>
2020-04-04 22:15:50 -0700

    got 2 projs

--- src/11ty/_data/portfolioList.js
+++ src/11ty/_data/portfolioList.js
@@ -1,34 +1,55 @@
 module.exports = [
 	{
+		id: "pugsum",
 		title: "Pugsum",
 		description:
 			"When you combine pug and possum(11ty) to make something awesome, a 11ty portfolio using Pug templates",
 		imageUrl: "/assets/images/pugsum/thumbnail.png",
-		url: "/pugsum",
+		url: "/project/pugsum",
 		bgColor: "bg-blue-200",
 	},
+	// {
+	// 	title: "NetBase Pro",
+	// 	description: "Event microsite for company's tradeshows and conferences",
+	// 	imageUrl:
+	// 		"https://www.mamp.info/__we_thumbs__/1284_4_MAMP-PRO-MacBook-Pro.png",
+	// 	url: "/netbase-pro",
+	// 	bgColor: "bg-red-200",
+	// },
 	{
-		title: "NetBase Pro",
-		description: "Event microsite for company's tradeshows and conferences",
-		imageUrl:
-			"https://www.mamp.info/__we_thumbs__/1284_4_MAMP-PRO-MacBook-Pro.png",
-		url: "/netbase-pro",
-		bgColor: "bg-red-200",
-	},
-	{
+		id: "dangfoods",
 		title: "Dangfoods",
-		description: "Event microsite for company's tradeshows and conferences",
-		imageUrl:
-			"https://www.mamp.info/__we_thumbs__/1284_4_MAMP-PRO-MacBook-Pro.png",
-		url: "/dangfoods",
-		bgColor: "bg-purple-200",
-	},
-	{
-		title: "Klipfolio Visualizations",
-		description: "Event microsite for company's tradeshows and conferences",
-		imageUrl:
-			"https://www.mamp.info/__we_thumbs__/1284_4_MAMP-PRO-MacBook-Pro.png",
-		url: "/klipfolio",
+		description:
+			"Custom WordPress Development for Dangfood's <span class='inline-block'>e-Commerce</span> site",
+		imageUrl: "/assets/images/dangfoods/thumbnail.png",
+		headerUrl: "/assets/images/dangfoods/header.jpg",
+		url: "/project/dangfoods",
 		bgColor: "bg-green-200",
+		role: [
+			"Front-End Developer",
+			"Redesigned the site based on design specs provided by client",
+			"Added Advanced Custom Fields for end-users to upload products with ease",
+		],
+		tech: [
+			"HTML5 / CSS3",
+			"WordPress",
+			"PHP",
+			"jQuery",
+			"Zurb Foundation",
+			"Adobe Photoshop",
+		],
+		details: [
+			`Dang Foods is a family-operated Asian snacks company located in the Bay Area. Their current setup was not working well for them. Even though it was hosted on WordPress, they were making  conent updates directly on the templates.`,
+			`As a team with another Freelance Developer, we refreshed the look of the website with the design specs provided by the client.
+			In the process, we also implemented Advanced Custom Fields and Custom Post Types to give a much cleaner backend experience. We completed the task in two weeks and the client was happy that he could make product updates with ease.`,
+		],
 	},
+	// {
+	// 	title: "Klipfolio Visualizations",
+	// 	description: "Event microsite for company's tradeshows and conferences",
+	// 	imageUrl:
+	// 		"https://www.mamp.info/__we_thumbs__/1284_4_MAMP-PRO-MacBook-Pro.png",
+	// 	url: "/klipfolio",
+	// 	bgColor: "bg-purple-200",
+	// },
 ];
