70ad58c3e48a22509dc368935fc32fabbb9d350b
Victor Tsang <victorwltsang@gmail.com>
2020-04-05 16:27:42 -0700

    fix ga

--- src/11ty/_includes/inline/ga.js
+++ src/11ty/_includes/inline/ga.js
@@ -1,23 +1,27 @@
-(function (i, s, o, g, r, a, m) {
-	console.log("ga loaded");
-	i["GoogleAnalyticsObject"] = r;
-	(i[r] =
-		i[r] ||
-		function () {
-			(i[r].q = i[r].q || []).push(arguments);
-		}),
-		(i[r].l = 1 * new Date());
-	i.initAnalytics = function () {
+// load ga after HTML is loaded, but also all the external resources: images, styles
+// micro-performance, only care about tracking users that actually stays after page is loaded
+document.addEventListener(
+	"load",
+	(function (i, s, o, g, r, a, m) {
+		i["GoogleAnalyticsObject"] = r;
+		(i[r] =
+			i[r] ||
+			function () {
+				(i[r].q = i[r].q || []).push(arguments);
+			}),
+			(i[r].l = 1 * new Date());
 		(a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
 		a.async = 1;
 		a.src = g;
 		m.parentNode.insertBefore(a, m);
-	};
-})(window, document, "script", "//www.google-analytics.com/analytics.js", "ga");
+	})(
+		window,
+		document,
+		"script",
+		"https://www.google-analytics.com/analytics.js",
+		"ga"
+	)
+);
 
 ga("create", "UA-131565495-1", "auto");
 ga("send", "pageview");
-
-// load ga after HTML is loaded, but also all the external resources: images, styles
-// micro-performance, only care about tracking users that actually stays after page is loaded
-document.addEventListener("load", initAnalytics);
