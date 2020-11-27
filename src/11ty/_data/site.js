b1bbed1986ef2a5000a3b1904faa9c771cbc616f
Victor Tsang <victorwltsang@gmail.com>
2020-11-27 15:04:15 -0800

    update url

--- src/11ty/_data/site.js
+++ src/11ty/_data/site.js
@@ -1,6 +1,6 @@
 module.exports = {
 	author: "Victor Tsang",
-	url: "https://vktr.me",
+	url: "https://vktr.dev",
 	metaTitle: "Victor Tsang",
 	metaDescription: "Welcome to my portfolio site!! Its built using 11ty, TailwindCSS and Pug.",
 	linkedIn: "https://www.linkedin.com/in/vktrwlt/",
